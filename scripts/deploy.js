9ef0425948ea1c7dfd7e1c385359755d859f67bc
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2022-09-19 21:57:39 -0400

    started on minting profile front end

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,6 +11,12 @@ async function main() {
   const feeAccount = await DefiBeats.feeAccount()
 
   console.log(`DefiBeats deployed to ${DefiBeats.address}, Fee Account ${feeAccount}, deployer ${deployer}`)
+
+  const profileNftFactory = await hre.ethers.getContractFactory("ProfileNFT")
+  const ProfileNFT = await profileNftFactory.deploy()
+  await ProfileNFT.deployed()
+
+  console.log(`Profile NFT Contract deployed to ${ProfileNFT.address}`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- src/components/CurrentUsersProfile.js
+++ src/components/CurrentUsersProfile.js
@@ -1,9 +1,31 @@
-
-
-
-import React from 'react'
+import { ethers } from 'ethers';
+import profileNftAbi from "../assets/profilenft.json"
 
 const CurrentUsersProfile = () => {
+
+  const PROFILENFT_ADDRESS=""
+
+  const mintProfilePic = async () =>{
+    try{
+      const {ethereum} = window;
+      if(ethereum){
+        const provider = new ethers.providers.Web3Provider(ethereum)
+        const signer = provider.getSigner()
+        const ProfileNFT = new ethers.Contract(PROFILENFT_ADDRESS, profileNftAbi.abi, signer)
+
+        console.log("Popping Metamask for gas fees")
+
+        ProfileNFT.mint()
+
+      }
+    }catch (error){
+      console.log(error)
+    }
+    
+  }
+
+
+
   return (
     <div>
         <div>
