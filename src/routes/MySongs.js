825bbd35438a67dca61969216efe35cd486a02e9
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2022-09-21 20:10:03 -0400

    scaffold for mysongs done

--- src/routes/MySongs.js
+++ src/routes/MySongs.js
@@ -1,10 +1,29 @@
 
 
-import React from 'react'
+import React, { useState } from 'react'
 
 const MySongs = () => {
+
+  const [mySongs, setMySongs] = useState([])
+
   return (
-    <div>MySongs</div>
+    <div id="content">
+      <div>
+        <h1>MySongs</h1>
+      </div>
+      <div>{mySongs.map((na, i)=>(
+        <div className='input-group mb-3' key={i}>
+            <p>{i} </p>
+            
+            <p>{i.image} </p>
+            <br/>
+            <button>Play</button>
+            <button>List For Sale</button>
+        </div>
+        
+    ))} 
+    </div>
+    </div>
   )
 }
 
