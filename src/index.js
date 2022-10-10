bed2de68894a0dd57f4444d79633f2af931933ce
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2022-10-10 11:58:44 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 import App from './App';
 import {store} from './redux/Store';
 
-
+// react
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
